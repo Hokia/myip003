3f4ca577e46dd6b5e83396e2a5a87293e5b5da75
Hokia <58064663+Hokia@users.noreply.github.com>
2024-12-23 16:15:01 +0800

    Update app.js

--- res/app.js
+++ res/app.js
@@ -2,7 +2,8 @@ new Vue({
     el: '#app',
     data: {
         // 请在此处填写 Bing Maps API Key，申请是免费的
-        bingMapAPIKEY: 'AIzaSyBp0Qkt1_XLzYZinO_A9fjwTOuKGrFWl6Y',
+        //bingMapAPIKEY: 'AIzaSyBp0Qkt1_XLzYZinO_A9fjwTOuKGrFWl6Y',
+        bingMapAPIKEY: 'Ap7Ph8ubijhKv0b9vhpWeRmlxFqEOZXCzeJg1m_jR6aaz6LQCcK4ombxy1Z5bpho',
         ipDataCards: [
             {
                 id: 'upai',
@@ -389,12 +390,12 @@ new Vue({
                     card.asnlink = false;
                     card.mapUrl = '';
                 } else {
-                    //card.asnlink = `https://radar.cloudflare.com/traffic/${card.asn}`;
-                    //card.mapUrl = `https://dev.virtualearth.net/REST/v1/Imagery/Map/Road/${card.latitude},${card.longitude}/5?mapSize=800,640&pp=${card.latitude},${card.longitude};66&key=${bingMapAPIKEY}&fmt=jpeg&dpi=Large`;
+                    card.asnlink = `https://radar.cloudflare.com/traffic/${card.asn}`;
+                    card.mapUrl = `https://dev.virtualearth.net/REST/v1/Imagery/Map/Road/${card.latitude},${card.longitude}/5?mapSize=800,640&pp=${card.latitude},${card.longitude};66&key=${bingMapAPIKEY}&fmt=jpeg&dpi=Large`;
 
                     // 可选改成 Google Maps 内嵌 iFrame
                     //card.mapUrl = `https://www.google.com/maps?q=${card.latitude},${card.longitude}&z=2&output=embed`;
-                    card.mapUrl = `https://www.google.com/maps/embed/v1/view?key=${bingMapAPIKEY}&center=${latitude},${longitude}&zoom=10`;
+                    //card.mapUrl = `https://www.google.com/maps/embed/v1/view?key=${bingMapAPIKEY}&center=${latitude},${longitude}&zoom=10`;
                 }
 
 
