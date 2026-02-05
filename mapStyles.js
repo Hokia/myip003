731676a14e87901162fc28f2d4f9f2049607c326
Hokia <58064663+Hokia@users.noreply.github.com>
2026-02-05 16:40:20 +0800

    Create mapStyles.js

---
+++ mapStyles.js
@@ -0,0 +1,58 @@
+window.MAP_STYLES = {
+  day: [
+    {
+      featureType: "administrative.land_parcel",
+      elementType: "labels",
+      stylers: [{ visibility: "off" }]
+    },
+    {
+      featureType: "poi",
+      elementType: "labels.text",
+      stylers: [{ visibility: "off" }]
+    },
+    {
+      featureType: "poi.business",
+      stylers: [{ visibility: "off" }]
+    },
+    {
+      featureType: "road",
+      elementType: "labels.icon",
+      stylers: [{ visibility: "off" }]
+    },
+    {
+      featureType: "road.local",
+      elementType: "labels",
+      stylers: [{ visibility: "off" }]
+    },
+    {
+      featureType: "transit",
+      stylers: [{ visibility: "off" }]
+    }
+  ],
+
+  night: [
+    { elementType: "geometry", stylers: [{ color: "#1d2c4d" }] },
+    { elementType: "labels.text.fill", stylers: [{ color: "#8ec3b9" }] },
+    { elementType: "labels.text.stroke", stylers: [{ color: "#1a3646" }] },
+    {
+      featureType: "administrative.country",
+      elementType: "geometry.stroke",
+      stylers: [{ color: "#4b6878" }]
+    },
+    {
+      featureType: "poi",
+      elementType: "labels.text",
+      stylers: [{ visibility: "off" }]
+    },
+    {
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [{ color: "#304a7d" }]
+    },
+    {
+      featureType: "water",
+      elementType: "geometry",
+      stylers: [{ color: "#0e1626" }]
+    }
+  ]
+};
